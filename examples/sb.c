6a1bb31bfbe6e0caf14f081b2d47622aac04f940
ThatOneGin <pedrodragoncrystal@gmail.com>
2025-08-02 22:10:04 -0300

    Update `sb.c` example.

--- examples/sb.c
+++ examples/sb.c
@@ -1,10 +1,31 @@
 #define CBONE_IMPL
 #include "cbone.h"
 
-int main() {
+/*
+** Simple function to concatenate an array of strings
+** sep being 0 means no separator needed
+*/
+cbone_string_builder concat_table(char **table, char sep) {
   cbone_string_builder sb = cbone_sb_new();
-  cbone_sb_sprintf(&sb, "Hello, %s!%c", "world", 10);
-  printf("%s", cbone_sb_cstr(&sb));
+  char **old = table;
+  char *elm = table[0];
+  while (elm != NULL) {
+    cbone_sb_sprintf(&sb, "%s", elm); /* put the content */
+    if (sep != 0) cbone_sb_char(&sb, sep); /* put the separator */
+    elm = *(++table); /* advance element */
+  }
+  table = old; /* restore table */
+  return sb;
+}
+
+int main() {
+  char *table[] = {
+    "Hello,",
+    "World!",
+    NULL
+  };
+  cbone_string_builder sb = concat_table(table, ' ');
+  printf("%s\n", cbone_sb_cstr(&sb));
   cbone_sb_free(&sb);
   return 0;
 }
